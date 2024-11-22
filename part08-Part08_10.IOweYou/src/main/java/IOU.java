e0dcfcad4f5b44468a7ebf29ddbcaa037ed119f4
Hachem S <s.hachem@murena.io>
2024-11-22 17:10:22 +0200

    Submit solution for Ex 08-10

---
+++ part08-Part08_10.IOweYou/src/main/java/IOU.java
@@ -0,0 +1,28 @@
+
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hachem
+ */
+public class IOU {
+    private HashMap<String, Double> debts;
+
+    public IOU() {
+        this.debts = new HashMap<>();
+    }
+    
+    public void setSum(String toWhom, double amount) {
+        debts.put(toWhom, amount);
+    }
+    
+    public double howMuchDoIOweTo(String toWhom) {
+        return debts.getOrDefault(toWhom, 0.0);
+    }
+}
