26de08e189b02325d711a62d7b2040229913de1b
Hachem S <s.hachem@murena.io>
2024-12-06 02:22:11 +0200

    Submit solution for Ex 10-01

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,7 +6,17 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> inputs = new ArrayList<>();
         // Write your program here
-
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            inputs.add(input);
+        }
+        double average = inputs.stream().mapToInt(value -> Integer.valueOf(value)).average().getAsDouble();
+        System.out.println(average);
     }
 }
