008e65936c8bf10e8f8f52f965cba084cf7f3312
Hachem S <s.hachem@murena.io>
2024-12-08 13:45:26 +0200

    Submit solution for Ex 10-06

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,14 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Integer> nums = new ArrayList<>();
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            if (input < 0) {
+                break;
+            }
+            nums.add(input);
+        }
+        nums.stream().filter(v -> v >= 1 && v <= 5).forEach(v -> System.out.println(v));
     }
 }
