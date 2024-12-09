39464344fb34679ace5275c4e79beea896c70c60
Hachem S <s.hachem@murena.io>
2024-12-09 06:05:42 +0200

    Submit solution for Ex 10-09

--- part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -11,7 +11,16 @@ public class ReadingFilesPerLine {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // test the method here
-
+        
     }
 
+    public static List<String> read(String file) {
+        ArrayList<String> lines = new ArrayList<>();
+        try {
+            Files.lines(Paths.get(file)).forEach(line -> lines.add(line));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return lines;
+    }
 }
