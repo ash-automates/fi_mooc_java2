d876111c7ea001dc09d30f7bd43939667f8a5494
Hachem S <s.hachem@murena.io>
2025-02-03 08:43:08 +0100

    Submit solution for Ex 10-16

---
+++ part10-Part10_16.EnumAndIterator/src/main/java/Person.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hachem
+ */
+public class Person {
+    private String name;
+    private Education education;
+
+    public Person(String name, Education education) {
+        this.name = name;
+        this.education = education;
+    }
+
+    public Education getEducation() {
+        return education;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return this.getName() + ", " + this.getEducation();
+    }
+}
