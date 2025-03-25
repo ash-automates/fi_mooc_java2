c13ae87b503a67d5dafb5f0b1e0118922266fa85
Hachem <s.hachem@murena.io>
2025-03-25 12:28:53 +0000

    Submit solution for Ex 12-03

--- part12-Part12_03.SumTheseForMe/src/main/java/Program.java
+++ part12-Part12_03.SumTheseForMe/src/main/java/Program.java
@@ -8,5 +8,18 @@ public class Program {
         // test your method here
 
     }
+    
+    public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest) {
+        fromWhere = fromWhere < 0 ? 0 : fromWhere;
+        toWhere = toWhere > array.length ? array.length : toWhere;
+        
+        int sum = 0;
+        for (int i = fromWhere; i < toWhere; i++) {
+            if (array[i] >= smallest && array[i] <= largest) {
+                sum += array[i];
+            }       
+        }
+        return sum;
+    }
 
 }
