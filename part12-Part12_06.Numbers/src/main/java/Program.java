b24979655d9ba978b084ccf873116528afe66733
Hachem <s.hachem@murena.io>
2025-04-06 00:57:29 +0000

    Submit solution for Ex 12-06

--- part12-Part12_06.Numbers/src/main/java/Program.java
+++ part12-Part12_06.Numbers/src/main/java/Program.java
@@ -6,6 +6,12 @@ public class Program {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("How many random numbers should be printed?");
+        int value = Integer.valueOf(scanner.nextLine());
+        Random rand = new Random();
+        for (int i = 0; i < value; i++) {
+            System.out.println(rand.nextInt(11));
+        }
     }
 
 }
