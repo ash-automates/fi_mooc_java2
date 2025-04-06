c7c8c3df0b2a8b271456015e7a8433bec3fbb5c8
Hachem <s.hachem@murena.io>
2025-04-06 19:21:56 +0100

    Submit solution for Ex 12-09

--- part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -5,4 +5,15 @@ public class Program {
         // Test your method here
     }
 
+    public static String arrayAsString(int[][] array) {
+        StringBuilder sb = new StringBuilder();
+        for (int[] is : array) {
+            for (int j = 0; j < is.length; j++) {
+                int k = is[j];
+                sb.append(k);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
 }
