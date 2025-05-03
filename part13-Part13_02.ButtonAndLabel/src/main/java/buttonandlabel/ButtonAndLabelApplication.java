d9974209188f373dd16943892840d473e27ba228
Hachem S <hachemsekki@my.uopeople.edu>
2025-05-03 08:04:41 +0100

    Submit solution for Ex 13-02

--- part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
+++ part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
@@ -1,11 +1,32 @@
 package buttonandlabel;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
 
-public class ButtonAndLabelApplication {
+
+public class ButtonAndLabelApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ButtonAndLabelApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        Button button = new Button("This is a button!");
+        Label text = new Label("This is some text");
+        
+        FlowPane componentsGroup = new FlowPane();
+        componentsGroup.getChildren().add(text);
+        componentsGroup.getChildren().add(button);
+        
+        Scene scene = new Scene(componentsGroup);
+        window.setScene(scene);
+        window.show();
     }
 
 }
