7d89ba3f09a179121ccbcc0084c8bb8d0b0520eb
Hachem S <hachemsekki@my.uopeople.edu>
2025-05-04 13:59:29 +0100

    Submit solution for Ex 13-05

--- part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13-Part13_05.TextStatistics/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,31 @@
 package textstatistics;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-public class TextStatisticsApplication {
+
+public class TextStatisticsApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        BorderPane layout = new BorderPane();
+        HBox hbox = new HBox(new Label("Letters: 0"), new Label("Words: 0"), new Label("The longest word is:"));
+        layout.setCenter(new TextArea());
+        layout.setBottom(hbox);
+        
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.show();
     }
 
 }
