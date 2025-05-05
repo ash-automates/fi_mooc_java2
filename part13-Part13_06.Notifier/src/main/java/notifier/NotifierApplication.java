1087455e8906b472302eef05b424fd9363114f20
Hachem S <hachemsekki@my.uopeople.edu>
2025-05-05 08:50:32 +0100

    Submit solution for Ex 13-06

--- part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
+++ part13-Part13_06.Notifier/src/main/java/notifier/NotifierApplication.java
@@ -1,11 +1,31 @@
 package notifier;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class NotifierApplication {
+
+public class NotifierApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(NotifierApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        Button button = new Button("Update");
+        TextField textfield = new TextField();
+        Label toUpdate = new Label();
+        button.setOnAction((event) -> toUpdate.setText(textfield.getText()));
+        VBox layout = new VBox(textfield, button, toUpdate);
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.show();
     }
 
 }
