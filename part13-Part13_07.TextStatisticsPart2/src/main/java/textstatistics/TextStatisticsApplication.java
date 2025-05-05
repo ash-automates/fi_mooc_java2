7d0b4c2888f0ea497e69d20ae7cb2469356a0b5a
Hachem S <hachemsekki@my.uopeople.edu>
2025-05-05 09:41:28 +0100

    Submit solution for Ex 13-07

--- part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,48 @@
 package textstatistics;
 
+import java.util.Arrays;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
-public class TextStatisticsApplication {
+
+public class TextStatisticsApplication extends Application {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) throws Exception {
+        BorderPane layout = new BorderPane();
+        Label lettersCount = new Label("Letters: 0");
+        Label wordsCount = new Label("Words: 0");
+        Label longestWord = new Label("The longest word is:");
+        HBox hbox = new HBox(lettersCount, wordsCount, longestWord);
+        TextArea toAnalyze = new TextArea();
+        toAnalyze.textProperty().addListener((change, oldValue, newValue) -> {
+            int characters = newValue.length();
+            String[] parts = newValue.split(" ");
+            int words = parts.length;
+            String longest = Arrays.stream(parts)
+                .sorted((s1, s2) -> s2.length() - s1.length())
+                .findFirst()
+                .get();
+            lettersCount.setText("Letters: " + characters);
+            wordsCount.setText("Words: " + words);
+            longestWord.setText("The longest word is: " + longest);
+        });
+        layout.setCenter(toAnalyze);
+        layout.setBottom(hbox);
+        
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.show();
     }
 
 }
